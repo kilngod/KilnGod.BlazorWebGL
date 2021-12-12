63d25ab25cfbffb908064c61754d48de38a980b2
kilngod <49618376+kilngod@users.noreply.github.com>
2021-12-12 11:26:29 -0800

    Add project files.

---
+++ KilnGod.BlazorWebGL.TestSample/Other/MandelbrotExtensions.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+
+namespace KilnGod.BlazorWebGL.TestSample.Other;
+
+public static class MandelbrotExtensions
+{
+
+
+
+    public static void CalcCPUParallel(int[] buffer, int width, int height, float left, float right, float bottom, float top, int max_iterations)
+    {
+        int icnt = width;
+
+        Parallel.For(0, icnt, i =>
+        {
+            for (int j = 0; j < height; j++)
+            {
+                int index = i + j * width;  // ILGPU-like index
+                    int img_x = index % width;
+                int img_y = index / width;
+
+                float x0 = left + img_x * (right - left) / width;
+                float y0 = bottom + img_y * (top - bottom) / height;
+                float x = 0.0f;
+                float y = 0.0f;
+                int iteration = 0;
+                while ((x * x + y * y < 2 * 2) && (iteration < max_iterations))
+                {
+                    float xtemp = x * x - y * y + x0;
+                    y = 2 * x * y + y0;
+                    x = xtemp;
+                    iteration += 1;
+                }
+                buffer[index] = iteration;
+            }
+        });
+    }
+
+    public static async Task Draw(BasicCanvas basicCanvas, int[] data, int width, int height, int iterations, Color color)
+    {
+
+        byte[] result = new byte[width * height * 4];
+
+        for (int i = 0; i < width * height; i++)
+        {
+            Color fillColor = color;
+
+
+            if (data[i] >= iterations)
+            {
+                fillColor = color;
+            }
+            else
+            {
+
+                int red = data[i] * 30 % 256;
+                int green = data[i] * 20 % 256;
+                int blue = data[i] * 50 % 256;
+
+                fillColor = Color.FromArgb(255, red, green, blue);
+
+            }
+
+
+            result[i * 4] = fillColor.R;
+            result[i * 4 + 1] = fillColor.G;
+            result[i * 4 + 2] = fillColor.B;
+            result[i * 4 + 3] = fillColor.A;
+
+        }
+
+        await basicCanvas.CreateImageDataCopyByteArray("Mandelbrot", width, height, result);
+        await basicCanvas.PutImageData("Mandelbrot", 0, 0);
+    }
+
+
+
+}
+
---
+++ KilnGod.BlazorWebGL/BasicCanvas.razor.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace KilnGod.BlazorWebGL
+{
+    public partial class BasicCanvas : ComponentBase, IAsyncDisposable
+    {
+#nullable disable
+        private IJSObjectReference asyncModule = null;
+
+        private IJSInProcessObjectReference module = null;
+
+        protected IJSRuntime _jsRuntime;
+
+        protected IJSInProcessRuntime _jsInProcessRuntime = null;
+
+        public event Action<BasicCanvas> CanvasInitComplete = null;
+
+#nullable enable
+
+
+        [Parameter]
+        public bool IsTransparent { get; set; } = false;
+
+        [Parameter]
+        public bool IsDesyncronized { get; set; } = false;
+
+        [Parameter]
+        public int Height { get; set; } = 600;
+
+        [Parameter]
+        public int Width { get; set; } = 800;
+
+        [Parameter]
+        public bool IsFullScreen { get; set; } = false;
+
+
+        [Parameter]
+        public string CanvasId { get; set; } = Guid.NewGuid().ToString();
+
+        protected ElementReference _canvasRef;
+
+        public ElementReference CanvasReference => this._canvasRef;
+
+        public bool IsWebAssembley { get { return (_jsRuntime is IJSInProcessRuntime); } }
+
+
+        [Inject]
+        public IJSRuntime jsRuntime
+        {
+            get
+            {
+                return _jsRuntime;
+            }
+            set
+            {
+                _jsRuntime = value;
+
+                if (IsWebAssembley)
+                {
+                    _jsInProcessRuntime = (IJSInProcessRuntime)value;
+                 
+                }
+            }
+        }
+
+
+
+
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await base.OnAfterRenderAsync(firstRender);
+            if (firstRender)
+            {
+
+                try
+                {
+                    if (IsWebAssembley)
+                    {
+                        
+                        module = await _jsInProcessRuntime.InvokeAsync<IJSInProcessObjectReference>("import", "./_content/KilnGod.BlazorWebGL/BasicCanvas.js");
+
+                        module.InvokeVoid("initializeBasicCanvas", CanvasId, IsWebAssembley, IsTransparent, IsDesyncronized);
+                    }
+                    else
+                    {
+                        asyncModule = await jsRuntime.InvokeAsync<IJSObjectReference>("import", "./_content/KilnGod.BlazorWebGL/BasicCanvas.js");
+
+                        await asyncModule.InvokeVoidAsync("initializeBasicCanvas", CanvasId, IsWebAssembley, IsTransparent, IsDesyncronized);
+
+                    }
+
+                    if (CanvasInitComplete != null)
+                        CanvasInitComplete(this);
+
+                }
+                catch (Exception ex)
+                {
+                    var crap = ex.Message;
+                }               
+
+            }        
+
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            if (asyncModule != null)
+            {
+                await asyncModule.DisposeAsync();
+            }
+            module?.Dispose();
+        }
+
+        public async ValueTask InjectScript(string scriptText)
+        {
+            if (module != null)
+            {
+                module.InvokeVoid("InjectScript", scriptText);
+            }
+            else
+            {
+                await asyncModule.InvokeVoidAsync("InjectScript", scriptText);
+            }
+        }
+
+        public async ValueTask SetFunction(string functionName, string functionText)
+        {
+            if (module != null)
+            {
+                module.InvokeVoid("InjectFunction", CanvasReference, functionName, functionText);
+            }
+            else
+            {
+                await asyncModule.InvokeVoidAsync("InjectFunction", CanvasReference, functionName, functionText);
+            }
+        }
+
+        public async ValueTask SetGlobalFunction(string globalFunctionName, params object?[]? args)
+        {
+            if (module != null)
+            {
+                module.InvokeVoid(globalFunctionName, CanvasReference, args);
+            }
+            else
+            {
+                await asyncModule.InvokeVoidAsync(globalFunctionName, CanvasReference, args);
+            }
+        }
+
+        public async ValueTask<T> GetGlobalFunction<T>(string globalFunctionName, params object?[]? args)
+        {
+            if (module != null)
+            {
+                return module.Invoke<T>(globalFunctionName, CanvasReference, args);
+            }
+            else
+            {
+                return await asyncModule.InvokeAsync<T>(globalFunctionName, CanvasReference, args);
+            }
+        }
+
+
+        public async ValueTask SetValueBasicContext(string ValueName, params object?[]? args)
+        {
+            if (module != null)
+            {
+                module.InvokeVoid("setValueBasicContext", CanvasReference, ValueName, args);
+            }
+            else
+            {                
+                await asyncModule.InvokeVoidAsync("setValueBasicContext", CanvasReference, ValueName, args);
+            }
+        }
+
+
+        public async ValueTask<T> GetValueBasicContext<T>(string ValueName, params object?[]? args)
+        {
+            if (module != null)
+            {
+                return module.Invoke<T>("getValueBasicContext", CanvasReference, ValueName, args);
+            }
+            else
+            {
+                return await asyncModule.InvokeAsync<T>("getValueBasicContext", CanvasReference, ValueName, args);
+            }
+        }
+
+
+
+        public async ValueTask SetFunctionBasicContext(string FunctionName, params object?[]? args)
+        {
+            if (module != null)
+            {
+                module.InvokeVoid("setFunctionBasicContext", CanvasReference, FunctionName, args);
+            }
+            else
+            {
+                await asyncModule.InvokeVoidAsync("setFunctionBasicContext", CanvasReference, FunctionName, args);
+            }
+        }
+
+
+        public async ValueTask<T> GetFunctionBasicContext<T>(string FunctionName, params object?[]? args)
+        {
+            if (module != null)
+            {
+                return module.Invoke<T>("getFunctionBasicContext", CanvasReference, FunctionName, args);
+            }
+            else
+            {
+                return await asyncModule.InvokeAsync<T>("getFunctionBasicContext", CanvasReference, FunctionName, args);
+            }
+        }
+
+
+        public async ValueTask SetFunctionDrawingBasis(string FunctionName, params object?[]? args)
+        {
+            if (module != null)
+            {
+                module.InvokeVoid("setFunctionDrawingBasis", CanvasReference, FunctionName, args);
+            }
+            else
+            {
+                await asyncModule.InvokeVoidAsync("setFunctionDrawingBasis", CanvasReference, FunctionName, args);
+            }
+        }
+
+
+        public async ValueTask<T> GetFunctionDrawingBasis<T>(string FunctionName, params object?[]? args)
+        {
+            if (module != null)
+            {
+                return module.Invoke<T>("getFunctionDrawingBasis", CanvasReference, FunctionName, args);
+            }
+            else
+            {
+                return await asyncModule.InvokeAsync<T>("getFunctionDrawingBasis", CanvasReference, FunctionName, args);
+            }
+        }
+
+    }
+}
